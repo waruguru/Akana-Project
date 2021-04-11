ec1406c5426298dc557896c84f53e53548289501
waruguru7991 <rachelmuriithi57@gmail.com>
2021-04-11 17:45:49 +0300

    Add function that gets DayOfBrth

--- scripts.js
+++ scripts.js
@@ -1,3 +1,17 @@
 
 let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+function getDayOfBirth(){
+ let dateInput = document.getElementById("date").value;
+ let date = new Date(dateInput);
+
+if (!!date.valueOf()) {
+    let year = date.getFullYear().toString();
+    let dayOfWeek = date.getDay();
+        return dayOfWeek;
+    } else {
+        alert("Please select your birthday.");
+        return;
+    }
+}
